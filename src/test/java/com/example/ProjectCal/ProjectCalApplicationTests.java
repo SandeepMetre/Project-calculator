adcf1636063ca49e3a0858ef09231d87ba1cea8d
sandeep423 <sandeepmehtre111@gmail.com>
2023-10-05 09:36:50 +0530

    first commit

---
+++ src/test/java/com/example/ProjectCal/ProjectCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectCal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
